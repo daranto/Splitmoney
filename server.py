bead57a473a3aa5e1bcf25ea9893c16a5dba806b
Daranto <xcode@x0g.de>
2026-07-01 09:17:34 +0200

    Bust asset cache for sorted people list

--- server.py
+++ server.py
@@ -320,7 +320,7 @@ class SplitMoneyHandler(BaseHTTPRequestHandler):
     self.send_response(HTTPStatus.OK)
     self.send_header("Content-Type", content_type)
     self.send_header("Content-Length", str(len(body)))
-    self.send_header("Cache-Control", "no-cache")
+    self.send_header("Cache-Control", "no-store")
     self.end_headers()
     self.wfile.write(body)
 
